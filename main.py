1928a4615bf786eb59dff47a0616eb34239a45db
ako1993 <andreasorourke@gmail.com>
2025-11-17 22:37:12 -0500

    Arguments can now be passed to the agent

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -7,12 +8,14 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
 
     client = genai.Client(api_key=api_key)
-
-    response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    )
-
-    print(response.text)
+    try:
+        response = client.models.generate_content(
+            model='gemini-2.0-flash-001', contents= sys.argv[1]
+        )
+        print(response.text)
+    except IndexError:
+        print("Error! You must pass a question as an argument to the program")
+        sys.exit(1)
 
     prompt_tokens = response.usage_metadata.prompt_token_count
     response_tokens = response.usage_metadata.candidates_token_count
