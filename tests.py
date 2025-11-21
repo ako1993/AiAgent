43521659cba0e88be384210f68663c36d46bc8d3
ako1993 <andreasorourke@gmail.com>
2025-11-21 13:10:08 -0500

    Finished get_file_content function

--- tests.py
+++ tests.py
@@ -1,9 +1,4 @@
-from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
-print(get_files_info("calculator", "."))
+print(get_file_content("calculator", "lorem.txt"))
 
-print(get_files_info("calculator", "pkg"))
-
-print(get_files_info("calculator", "/bin"))
-
-print(get_files_info("calculator", "../"))
