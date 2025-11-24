75d309b7b4be060afc7d433f50c65cdd37c828a4
andreas <andre@LAPTOP-61UG31CH.localdomain>
2025-11-23 21:44:34 -0500

    Commiting changes before I extract generate content into it's own function

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import os
-import sys
 import argparse
+
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+
+from config import system_prompt, available_functions
+
 
 def main():
     load_dotenv()
@@ -17,11 +20,16 @@ def main():
 
 
     messages = [types.Content(role = "user", parts=[types.Part(text = args.prompt)])]
-    response = client.models.generate_content(model='gemini-2.0-flash-001', contents= messages)
+    response = client.models.generate_content(model='gemini-2.0-flash-001',
+                                               contents= messages,
+                                               config=types.GenerateContentConfig(
+                                                   tools=[available_functions], system_instruction=system_prompt))
 
     prompt_tokens = response.usage_metadata.prompt_token_count
     response_tokens = response.usage_metadata.candidates_token_count
-
+    if response.function_calls:
+        for function_call in response.function_calls:
+            print(f"Calling function {function_call.name}{function_call.args}")
     print(response.text)
     if args.verbose:
         print(f'User prompt: {args.prompt}')
